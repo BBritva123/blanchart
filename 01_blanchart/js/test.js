cb83c3b22540cfa5540b0e23150c0420be8856da
Sergey Vedenyapin <odnako_mi@mail.ru>
2022-07-27 12:49:07 +0300

    swiper pixel perfect, not modal

---
+++ 01_blanchart/js/test.js
@@ -0,0 +1,15 @@
+// function openBurger() {
+//     let button = document.querySelector('.header-top__button-burger');
+//     let burgerMenu = document.querySelector('.header-top__login-block');
+//     let list = document.querySelector('.header-top__list');
+//     let loginLink = document.querySelector('.header-top__login');
+//     button.addEventListener('click', () => {
+//         button.classList.toggle('burger-active');
+//         list.classList.toggle('display-active_flex');
+//         loginLink.classList.toggle('display-active_flex')
+//     })
+//     console.log(button);
+//     console.log(burgerMenu);
+// };
+// openBurger();
+
